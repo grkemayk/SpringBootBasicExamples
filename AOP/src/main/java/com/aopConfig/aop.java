5948ae69125340df0aa7ca9655a38452f1495470
grkemayk <93814922+grkemayk@users.noreply.github.com>
2023-01-18 12:53:01 +0300

    examples

--- AOP/src/main/java/com/aopConfig/aop.java
+++ AOP/src/main/java/com/aopConfig/aop.java
@@ -2,24 +2,38 @@ package com.aopConfig;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.springframework.context.annotation.Configuration;
 
 @Aspect
 @Configuration
 public class aop {
-    @Before("execution(* com.controller.aopCOntroller.*(..)) && @annotation(sampleAnnotation)")
+    /*@Before("execution(* com.controller.aopCOntroller.*(..)) && @annotation(sampleAnnotation)")
     public void before1(JoinPoint jp, myAn sampleAnnotation){
         System.out.println(sampleAnnotation.name());
         System.out.println("before method");
-    }
-    @Around("execution(* com.controller.aopCOntroller.*(..)) && @annotation(sampleAnnotation)")
-    public void before2(ProceedingJoinPoint jp, myAn sampleAnnotation) throws Throwable {//ProceedingJoinPoint.proceed() return value'yu alır.
+    }*/
+
+    /*@After("execution(* com.controller.aopCOntroller.*(..)) && @annotation(sampleAnnotation)")
+    public void after1(JoinPoint jp, myAn sampleAnnotation){
+        System.out.println(sampleAnnotation.name());
+        System.out.println("before method");
+    }*/
+
+    /*@Around("execution(* com.controller.aopCOntroller.*(..)) && @annotation(sampleAnnotation)")
+    public void around1(ProceedingJoinPoint jp, myAn sampleAnnotation) throws Throwable {//ProceedingJoinPoint.proceed() return value'yu alır.
         System.out.println(jp.proceed());
         System.out.println(sampleAnnotation.name());
         System.out.println("after method");
-    }
+    }*/
+
+    /*@AfterReturning("execution(* com.controller.aopCOntroller.*(..))")
+    public void afterReturning1(JoinPoint jp){//ProceedingJoinPoint.proceed() return value'yu alır.
+        System.out.println("after method");
+    }*/
+
+    /*@AfterThrowing("execution(* com.controller.aopCOntroller.*(..))")
+    public void afterThrowing1(JoinPoint jp){//ProceedingJoinPoint.proceed() return value'yu alır.
+        System.out.println("after method");
+    }*/
 }
